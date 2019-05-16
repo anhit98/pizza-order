f8c345d56aacc7c45f7ffec06372b875916f0a44
Anh Do Thi Ngoc <anh.dothingoc@imac-0021.pyco.vn>
2019-05-16 12:39:06 +0700

    connect to mlab

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 const mongoose  = require("mongoose");
-const url = "mongodb://localhost:27017/order";
+
+const url = "mongodb+srv://NHI-NGUYEN_97:nhinguyen040197@@cluster0-odomf.mongodb.net/order?retryWrites=true";
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
