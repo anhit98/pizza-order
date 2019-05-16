dd7c92fc16ddc1e80cfa4738cdfd24470c1bf28a
Anh Do Thi Ngoc <anh.dothingoc@imac-0021.pyco.vn>
2019-05-16 10:16:50 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,42 @@
-const http = require('http');
+'use strict'; 
+const Hapi = require('hapi'); 
+const Inert = require('inert');
+const Vision = require('vision');
+const fs   = require('fs');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./../package');
+var publicKEY  = fs.readFileSync('public.key', 'utf8');
+require('dotenv').config()
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+const server = Hapi.server({ port: port}); 
+
+
+const init = async () => {
+    
+    const swaggerOptions = {
+            info: {
+                    title: 'Test API Documentation',
+                    version: Pack.version,
+                },
+            };
+        
+        await server.register([
+            Inert,
+            Vision,
+            {
+                plugin: HapiSwagger,
+                options: swaggerOptions
+            }
+        ]);
+        await server.start(); 
+        console.log(`Server running at: ${server.info.uri}`); 
+        server.route(require('./../src/routes/category'));
+        server.route(require('./../src/routes/product'));
+}; 
+
+init().catch(err => {
+    console.error('An error happened while initializing the server', {
+        err,
+    });
+    process.exit(1);
 });
