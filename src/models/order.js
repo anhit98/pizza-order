ea21a3eeb4c3e31bf595331acaa7fe2509c4c714
Anh Do Thi Ngoc <anh.dothingoc@imac-0021.pyco.vn>
2019-05-16 10:08:43 +0700

    commit

--- src/models/order.js
+++ src/models/order.js
@@ -1,10 +1,41 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    date: {
+        type: Date, 
+        default: Date.now
+      },
+      customer_id: {
+        type: String,
+        required: [true, 'userId is required']
+      },
+      
+      order: [{
+        product_id: String,
+        quantity: String
+     }],
+     
+      orderStatus: { 
+        type: String,
+        required: [true, 'status is required'],
+        enum : ['ordered','pending','in-transit','completed'],
+        default: 'ordered'
+      }
+    
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+const OrderModel = mongoose.model('Order', orderSchema);
+
+  const createOrder =  (order,cb) =>  OrderModel.create(order,cb);
+  const updateOrder =  (id, data, cb) =>  OrderModel.findByIdAndUpdate({_id:id}, data,{new : true} , cb);
+  const findOrderById =  (idOrder, cb) =>  OrderModel.find({_id:idOrder}, cb);
+  const findOrderByStatus =  (statusOrder, cb) =>  OrderModel.find(statusOrder, cb);
+
+module.exports = {
+  createOrder,
+  updateOrder,
+  findOrderById,
+  findOrderByStatus
+}
